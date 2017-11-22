7b44749331cd209552e6f6c3fca60de6b25d7974
sanjeev <chopra@ge.com>
2017-11-21 16:41:03 -0800

    Added tests for DefaultZoneConfiguration and removed redundant issuer field.

--- src/test/java/com/ge/predix/uaa/token/lib/DefaultZoneConfiguratorTest.java
+++ src/test/java/com/ge/predix/uaa/token/lib/DefaultZoneConfiguratorTest.java
@@ -18,6 +18,7 @@ package com.ge.predix.uaa.token.lib;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import org.testng.Assert;
@@ -27,49 +28,92 @@ import org.testng.annotations.Test;
 @Test
 public class DefaultZoneConfiguratorTest {
 
-    @Test(dataProvider = "validAllowedPatters")
-    public void testValidAllowedPatters(final List<String> allowedUriPatterns) {
+    @Test(dataProvider = "validAllowedPatterns")
+    public void testValidAllowedPatterns(final List<String> allowedUriPatterns) {
         DefaultZoneConfiguration configurator = new DefaultZoneConfiguration();
         configurator.setAllowedUriPatterns(allowedUriPatterns);
 
         Assert.assertEquals(configurator.getAllowedUriPatterns(), allowedUriPatterns);
     }
 
-    @Test(dataProvider = "invalidAllowedPatters", expectedExceptions = IllegalArgumentException.class)
-    public void testInvalidAllowedPatters(final List<String> allowedUriPatterns) {
+    @Test(dataProvider = "invalidAllowedPatterns", expectedExceptions = IllegalArgumentException.class)
+    public void testInvalidAllowedPatterns(final List<String> allowedUriPatterns) {
         DefaultZoneConfiguration configurator = new DefaultZoneConfiguration();
         configurator.setAllowedUriPatterns(allowedUriPatterns);
     }
 
-    public void testSingleTrustedIssuer() {
+    public void testSetTrustedIssuerId() {
         String expectedIssuer = "http://uaa.predix.com";
         DefaultZoneConfiguration configurator = new DefaultZoneConfiguration();
         configurator.setTrustedIssuerId(expectedIssuer);
 
         Assert.assertEquals(configurator.getTrustedIssuerId(), expectedIssuer);
-        // asserting default behavior for existing single issuer contract
+        Assert.assertEquals(configurator.getTrustedIssuerIds().size(), 1);
         Assert.assertEquals(configurator.getTrustedIssuerIds().iterator().next(), expectedIssuer);
     }
 
+    public void testSetTrustedIssuerIdNull() {
+        DefaultZoneConfiguration configurator = new DefaultZoneConfiguration();
+        configurator.setTrustedIssuerId(null);
+
+        Assert.assertEquals(configurator.getTrustedIssuerId(), null);
+        Assert.assertEquals(configurator.getTrustedIssuerIds().size(), 0);
+    }
+
+    public void testSetTrustedIssuerIdNullResetsIssuerIds() {
+        List<String> expectedIssuers = Arrays.asList("http://uaa.predix.com", "http://zac-uaa.predix.com");
+        DefaultZoneConfiguration configurator = new DefaultZoneConfiguration();
+        configurator.setTrustedIssuerIds(expectedIssuers);
+        Assert.assertEquals(configurator.getTrustedIssuerIds(), expectedIssuers);
+        
+        //reset
+        configurator.setTrustedIssuerId(null);
+
+        Assert.assertEquals(configurator.getTrustedIssuerId(), null);
+        Assert.assertEquals(configurator.getTrustedIssuerIds().size(), 0);
+    }
+    
     public void testMultipleTrustedIssuers() {
         List<String> expectedIssuers = Arrays.asList("http://uaa.predix.com", "http://zac-uaa.predix.com");
         DefaultZoneConfiguration configurator = new DefaultZoneConfiguration();
         configurator.setTrustedIssuerIds(expectedIssuers);
 
         Assert.assertEquals(configurator.getTrustedIssuerIds(), expectedIssuers);
-        // asserting default behavior for existing single issuer contract
         Assert.assertEquals(configurator.getTrustedIssuerId(), expectedIssuers.get(0));
     }
 
+    public void testSetTrustedIssuerIdsNull() {
+        DefaultZoneConfiguration configurator = new DefaultZoneConfiguration();
+        configurator.setTrustedIssuerIds(null);
+
+        Assert.assertEquals(configurator.getTrustedIssuerIds(), Collections.emptyList());
+        Assert.assertEquals(configurator.getTrustedIssuerId(), null);
+    }
+
+    public void testSetTrustedIssuerIdsEmpty() {
+        DefaultZoneConfiguration configurator = new DefaultZoneConfiguration();
+        configurator.setTrustedIssuerIds(new ArrayList<String>());
+
+        Assert.assertEquals(configurator.getTrustedIssuerIds(), Collections.emptyList());
+        Assert.assertEquals(configurator.getTrustedIssuerId(), null);
+    }
+
+    public void testSetTrustedIssuerIdsNotInitialized() {
+        DefaultZoneConfiguration configurator = new DefaultZoneConfiguration();
+
+        Assert.assertEquals(configurator.getTrustedIssuerIds(), Collections.emptyList());
+        Assert.assertEquals(configurator.getTrustedIssuerId(), null);
+    }
+
     @DataProvider
-    private Object[][] validAllowedPatters() {
+    private Object[][] validAllowedPatterns() {
 
         return new Object[][] { { new ArrayList<String>() }, { Arrays.asList("/zone/**") },
                 { Arrays.asList("/zone/?") }, };
     }
 
     @DataProvider
-    private Object[][] invalidAllowedPatters() {
+    private Object[][] invalidAllowedPatterns() {
 
         return new Object[][] { { Arrays.asList("/zone") }, { Arrays.asList("abc") }, };
     }
